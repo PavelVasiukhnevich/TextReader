7c24b1853f61c86b472457dbdf3d140b722615ba
Pavel <Pavel@SUPERPAVEL228>
2021-06-25 18:28:20 +0300

    Добавьте файлы проекта.

---
+++ TextReader/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace TextReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button4, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 94.12752F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.872483F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1243, 596);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 564);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(615, 29);
+            this.panel1.TabIndex = 15;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(187, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Сканировать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(94, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(615, 555);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Location = new System.Drawing.Point(624, 3);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(616, 555);
+            this.richTextBox2.TabIndex = 12;
+            this.richTextBox2.Text = "";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(624, 564);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Сохранить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1243, 596);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "TextReader";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ TextReader/Form1.cs
@@ -0,0 +1,273 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+using System.Text.Json.Serialization;
+
+namespace TextReader
+{
+    
+    public partial class Form1 : Form
+    {
+        class bigData
+        {
+            public string fileName { get; set; }
+            public int fileSize { get; set; }
+            public int lettersCount { get; set; }
+            public Dictionary<string, int> letters { get; set; }
+            public int wordsCount { get; set; }
+            public Dictionary<string, int> words { get; set; }
+            public int numbersCount { get; set; }
+            public Dictionary<string, int> numbers { get; set; }
+            public int numeralsCount { get; set; }
+            public Dictionary<string, int> numerals { get; set; }
+            public int linesCount { get; set; }
+            public string longestWord { get; set; }
+            public int wordsWithHyphen { get; set; }
+            public int punctuations { get; set; }
+            public int spaceCount { get; set; }
+            public int hyphenCount { get; set; }
+            public void Clear()
+            {
+                fileName = "";
+                fileSize = 0;
+                lettersCount = 1;
+                letters.Clear();
+                wordsCount = 0;
+                words.Clear();
+                numbers.Clear();
+                numbersCount = 0;
+                numeralsCount = 0;
+                numerals.Clear();
+                linesCount = 0;
+                longestWord = "";
+                wordsWithHyphen = 0;
+                punctuations = 0;
+                spaceCount = 0;
+                hyphenCount = 0;
+            }
+            public void Start()
+            {
+                linesCount = 1;
+                letters = new Dictionary<string, int>();
+                words = new Dictionary<string, int>();
+                numbers = new Dictionary<string, int>();
+                numerals = new Dictionary<string, int>();
+            }
+            public void Sort()
+            {
+                letters = letters.OrderByDescending(pair => pair.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
+                words = words.OrderByDescending(pair => pair.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
+                numbers = numbers.OrderByDescending(pair => pair.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
+                numerals = numerals.OrderByDescending(pair => pair.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
+            }
+            public void Sum()
+            {
+                lettersCount = letters.Sum(x => x.Value);
+                numeralsCount = numerals.Sum(x => x.Value);
+                wordsCount = words.Sum(x => x.Value);
+                numbersCount = numbers.Sum(x => x.Value);
+            }
+        }
+        bigData _Data;                  //Тут хранятся все собранные вместе и отсортированные данные
+        string allText = "";
+        public Form1()
+        {
+            InitializeComponent();
+            _Data = new bigData();
+            _Data.Start();
+        }
+        public void ClearMemory()
+        {
+            long totalMemory = GC.GetTotalMemory(false);
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+        private void button3_Click(object sender, EventArgs e) //Scan File
+        {
+            richTextBox2.Clear();
+            _Data.Clear(); ;
+            _Data.fileName = FileName(openFileDialog1.FileName);
+            _Data.fileSize = allText.Length;
+            string json="";
+            try
+            {
+                textReader(richTextBox1.Text);
+                 json = JsonConvert.SerializeObject(_Data, Formatting.Indented);
+            }
+            catch { }
+            richTextBox2.Text=json;
+            ClearMemory();
+        }
+        public string FileName(string name)
+        {
+            int index=0;
+            string backName = "";
+            for(int i=name.Length-1;i>-1; i--)
+            {
+                if (name[i].ToString()=="\\")
+                { index = i;
+                    break;
+                }
+            }
+            for (int i = index+1; i < name.Length; i++)
+                backName = backName + name[i];
+            return backName;
+        }//Процедура для нахождения имени файла
+        private void button1_Click(object sender, EventArgs e) //Open File
+        {
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt;";
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = openFileDialog1.FileName;
+            // читаем файл в строку
+            allText = System.IO.File.ReadAllText(filename);
+            richTextBox1.Text = allText;
+            ClearMemory();
+        }
+        private void button2_Click(object sender, EventArgs e) //ClearFile
+        {
+            richTextBox1.Clear();
+            richTextBox2.Clear();
+            _Data.Clear();
+        }
+        private  void button4_Click(object sender, EventArgs e) //Safe File
+        {
+            saveFileDialog1.Filter = "json files (*.json)|*.json;";
+            saveFileDialog1.FileName = "Result";
+            saveFileDialog1.InitialDirectory = "C:\\";
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            // получаем выбранный файл
+            string filename = saveFileDialog1.FileName;
+            // сохраняем текст в файл
+            string json = JsonConvert.SerializeObject(_Data, Formatting.Indented);
+            File.WriteAllText(filename, json);
+            MessageBox.Show("Файл сохранен");
+        }
+        public void textReader(string text)
+        {
+            int space1 = -1;
+            int space2 = 0;
+            string word="";
+            bool isWord = false;
+            bool isHyphen = false;
+            for (int i = 0; i < text.Length; i++)
+            {
+                //Работа с буквами
+                if (char.IsLetter(text[i]) && _Data.letters.ContainsKey(text[i].ToString()))        
+                {
+                    _Data.letters[text[i].ToString()] = _Data.letters[text[i].ToString()] + 1;
+                }//Проверка есть ли буква в списке, если есть то +1 
+                else if(char.IsLetter(text[i]) && _Data.letters.ContainsKey(text[i].ToString())==false)
+                {
+                    _Data.letters.Add(text[i].ToString(), 1);
+                } //Проверка есть ли буква в списке, если нет - добавить в список     
+
+                //Работа с цифрами
+                if (char.IsNumber(text[i]) && _Data.numerals.ContainsKey(text[i].ToString()))
+                {
+                    _Data.numerals[text[i].ToString()] = _Data.numerals[text[i].ToString()] + 1;
+                }//Проверка есть ли цифра в списке, если есть то +1 
+                else if (char.IsNumber(text[i]) && _Data.numerals.ContainsKey(text[i].ToString())==false)
+                {
+                    _Data.numerals.Add(text[i].ToString(), 1);
+                } //Проверка есть ли цифра в списке, если нет - добавить в список     
+
+                //Работа с словами, словами, дефисами и числами
+                if (i > space1&&char.IsLetter(text[i]) && space2 == 0
+                    ||text[i].ToString()=="-"&& i > space1 && space2 == 0
+                    ||char.IsPunctuation(text[i]) && i > space1 && space2 == 0)
+                {
+                    isWord = true;
+                }//Проверка есть ли хоть одна буква, дефис или знак препинания в слове
+                if ((i > space1 && text[i].ToString()==("-") && space2 == 0))
+                {
+                    isHyphen = true;
+                }//Проверка есть ли в слове дефис
+                if (space2==0 &&i>space1&& char.IsWhiteSpace(text[i])!=true&&text[i].ToString()!=("\\r"))
+                {
+                    word = word + text[i];
+                }   //Добавление буквы в слово
+                if(char.IsWhiteSpace(text[i])&&space1==i-1|| text[i].ToString() == ("\\r") && space1 == i - 1)
+                {
+                    space1++; 
+                }   //Проверка двойных пробелов
+                if (char.IsWhiteSpace(text[i]) && i > space1 || text[i].ToString() == ("\\r") && i > space1  
+                    || i==text.Length-1 && i > space1)
+                {
+                    space2 = i;
+                }//Находим второй пробел (учитывая перенос строки,конец текста и двойнные пробелы)
+                if (space2>0)
+                {
+                    if (isWord)
+                    {
+                        if (_Data.words.ContainsKey(word))
+                        {
+                            _Data.words[word] = _Data.words[word] + 1;
+                        }
+                        else
+                            _Data.words.Add(word, 1);
+                        if (word.Length > _Data.longestWord.Length)
+                            _Data.longestWord = word;                 //Проверка на самое длинное слово
+                        if (isHyphen) { _Data.wordsWithHyphen++;}    //Проверка на дефис
+                    }
+                    if (isWord==false)
+                    {
+                        if (_Data.numbers.ContainsKey(word))
+                        {
+                            _Data.numbers[word] = _Data.numbers[word] + 1;
+                        }
+                        else
+                            _Data.numbers.Add(word, 1);
+                    }
+                    space1 = space2;
+                    space2 = 0;
+                    isWord = false;
+                    isHyphen = false;
+                    word = "";
+                } //записываем слово или число в массив и обнуляем второй пробел, поле isWord и isHyphen
+
+                //Работа со знаками пунктуации
+                if (char.IsPunctuation(text[i]))
+                {
+                    _Data.punctuations++;
+                }     //Поиск количества знаков припинания
+                if( text[i].ToString() == ("\n"))
+                {
+                    //Environment.NewLine
+                    _Data.linesCount++;
+                }    //Поиск количества строк
+                if (text[i].ToString() == " ")
+                {
+                    _Data.spaceCount++;
+                }       //Поиск количества пробелов
+                if(text[i].ToString()=="-")
+                {
+                    _Data.hyphenCount++;
+                }          //Проверка количества дефисов
+            }
+            _Data.linesCount++;
+            _Data.fileSize = text.Length;
+            _Data.Sum();
+            _Data.Sort();
+        }//Процедура для сканирования всего текста
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            allText = richTextBox1.Text;
+        }
+    }
+}
+
